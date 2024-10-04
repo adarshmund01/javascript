4d7996462bf8d1bcc2da00057d0fe703c475a294
Adarsh Mund <adarshmund6@.com>
2024-10-05 01:29:21 +0530

    more of Data types

---
+++ ConserversionOperation.js
@@ -0,0 +1,31 @@
+// //let score ="55abc"
+
+
+// console.log(typeof score)
+
+// let valueInNumber=Number (score)
+ 
+// //creating this specificly for number 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+  
+
+// let isLoggedIn = "abc"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//1 =>true , 0 => false 
+//""  empty string =>false
+//"abc" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
