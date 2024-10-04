732a5615b54309fbe84d132cd152e9ebf8fac8a2
Adarsh Mund <adarshmund6@.com>
2024-10-04 20:12:07 +0530

    all about variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId = "123456"
+let accountEmail ="adarsh@mund.com"
+let accountpassword = "ThinkPad@11"
+accountCity = "jaipur"
+
+
+console.table([accountId, accountEmail, accountpassword, accountCity])
