abe89ce08e998d0e7549f3d0ce89f2d3fceb2d45
Adarsh Mund <adarshmund6@.com>
2024-10-04 20:50:05 +0530

    Data Types

---
+++ 02-Data_Types.js
@@ -0,0 +1,17 @@
+console.log(3 + 4)
+
+let name = "adarsh"
+let age ="30"
+let isLoggedIn = false
+let state = null
+
+// Data Types 
+// 1- Number
+// 2- String
+// 3-Boolean
+// 4-null
+// 5-undefined
+// 6-Symbol
+
+console.log(typeof null)// it refred to object
+console.log(typeof undefined)// it refred to undefined
