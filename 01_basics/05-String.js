64993278f299c4fed90ccedb11ecb61033503579
Adarsh Mund <adarshmund6@.com>
2024-10-19 16:43:50 +0530

    depth of string methods

--- 01_basics/05-String.js
+++ 01_basics/05-String.js
@@ -1,6 +1,16 @@
+//--There is two way to declared string
+//-1st
 const name ="adarsh"
 const repoCount = 50
 
 //console.log(name + repoCount)
 
-console.log(`hello my name is ${name} and i am a software developer in ${repoCount}`);
+//console.log(`hello my name is ${name} and i am a software developer in ${repoCount}`);
+
+//-2nd using constractor
+const gamName = new String('adarsh')
+console.log(gamName[0]);
+
+const onelifeBaby =" Akash "
+console.log(onelifeBaby)
+console.log(onelifeBaby.trim());
