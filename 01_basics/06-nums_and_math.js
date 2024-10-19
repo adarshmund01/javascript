0131337884e7314cd82ec5ae8025f7ea252d25c5
Adarsh Mund <adarshmund6@.com>
2024-10-20 00:17:55 +0530

    numbers and maths

---
+++ 01_basics/06-nums_and_math.js
@@ -0,0 +1,17 @@
+//******************Numbers********************** *
+//const score = 100
+
+//console.log(score);
+
+const score = new Number(100)
+//console.log(score);
+//console.log(score.toString().length);
+//console.log(score.toString().length);
+//console.log(score.toFixed(2));
+
+//How to convert a string into indian number counting style with , ,
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//********************Maths *****************************
+
