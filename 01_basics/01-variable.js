da80539761fa12ac942660c93b095a038b667fb2
Adarsh Mund <adarshmund6@.com>
2024-12-30 12:00:24 +0530

    let comments

--- 01_basics/01-variable.js
+++ 01_basics/01-variable.js
@@ -7,6 +7,11 @@ accountCity = "jaipur"
 console.table([accountId, accountEmail, accountpassword, accountCity])
 
 /*
+Note: accountCity = "jaipur"---- we can declared variable like this also without let keyword 
+but this is not recomanded
+*/
+
+/*
 prefer not to use var-keyword
 because of issue in block scope and functional scope 
 */
