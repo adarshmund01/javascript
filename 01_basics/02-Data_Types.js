124032fb722de685196067b3e4820ea26f07bb95
Adarsh Mund <adarshmund6@.com>
2025-03-04 02:47:44 +0530

    adding more comments

--- 01_basics/02-Data_Types.js
+++ 01_basics/02-Data_Types.js
@@ -3,9 +3,10 @@ console.log(3 + 4)
 let name = "adarsh"
 let age ="30"
 let isLoggedIn = false
-let state = null
+let state = null// it will come as object
 //let Number; - it will come as undefined
 let city =""// it will come as string with no value as we have declared it with ("") empty string
+var x;
 
 // Data Types 
 // 1- Number
@@ -18,3 +19,5 @@ let city =""// it will come as string with no value as we have declared it with 
 console.log(typeof null)// it refred to object
 console.log(typeof undefined)// it refred to undefined
 console.log(typeof city)// it refred to string
+console.log(typeof x)
+console.log(10 + "5")// it will come as 105 as it will concatinate the string
