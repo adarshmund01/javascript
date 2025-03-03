9a36b703acddfd43dcb4a49a83a6936944c7f44a
Adarsh Mund <adarshmund6@.com>
2025-03-04 02:03:30 +0530

    few extra info added

--- 01_basics/02-Data_Types.js
+++ 01_basics/02-Data_Types.js
@@ -4,6 +4,8 @@ let name = "adarsh"
 let age ="30"
 let isLoggedIn = false
 let state = null
+//let Number; - it will come as undefined
+let city =""// it will come as string with no value as we have declared it with ("") empty string
 
 // Data Types 
 // 1- Number
@@ -15,3 +17,4 @@ let state = null
 
 console.log(typeof null)// it refred to object
 console.log(typeof undefined)// it refred to undefined
+console.log(typeof city)// it refred to string
